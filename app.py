26f993d7a5e76200ab08497001ece271bea80cd4
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-10-21 17:30:25 +0000

    html

--- app.py
+++ app.py
@@ -10,3 +10,21 @@ from flask import Flask, render_template, current_app, request, redirect, url_fo
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'asdasdasd'
 Bootstrap(app)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    
+    return render_template("home.html")
+
+
+
+
+
+## APP INITIATION
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True) 
