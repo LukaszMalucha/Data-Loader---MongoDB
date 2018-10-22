e6b207a1e29ccc14a2ba21843c613fce5e355eae
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-10-22 17:36:19 +0000

    add functionality

--- app.py
+++ app.py
@@ -1,22 +1,40 @@
 ##### App Utilities
 import os
+import env
 from flask_bootstrap import Bootstrap
 from flask import Flask, render_template, current_app, request, redirect, url_for, flash
-
+from flask_pymongo import PyMongo
 
 
 ##### App Settings
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'asdasdasd'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY") 
 Bootstrap(app)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME") 
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI") 
+
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-@app.route('/home')
-def home():
+# @app.route('/home')
+# def home():
     
-    return render_template("home.html")
+#     return render_template("home.html")
+@app.route('/get_tweets')
+def get_tweets():
+    return render_template("tweets.html", tweets=mongo.db.tweets.find())
+    
+    
+@app.route('/add_tweet')
+def add_tweets():
+    
+    return render_template("add_tweets.html")
+    
 
 
 
@@ -33,4 +51,4 @@ def home():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=False)    
+            debug=True)    
