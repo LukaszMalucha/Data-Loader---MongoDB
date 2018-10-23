adfcb2866c2ade713a291aee3b9f2afc70933d2f
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-10-23 15:00:57 +0000

    form

--- app.py
+++ app.py
@@ -27,28 +27,30 @@ mongo = PyMongo(app)
 #     return render_template("home.html")
 @app.route('/get_tweets')
 def get_tweets():
-    return render_template("tweets.html", tweets=mongo.db.tweets.find())
+    _tweets=mongo.db.tweets.find()
+    tweets_list = [tweet for tweet in _tweets]
+    return render_template("get_tweets.html", tweets=tweets_list)
     
     
 @app.route('/add_tweet')
 def add_tweets():
+    _hashtags = mongo.db.hashtags.find()
+    hashtag_list = [hashtag for hashtag in _hashtags]
+    return render_template("add_tweets.html", hashtags= hashtag_list)
     
-    return render_template("add_tweets.html")
-    
-
+@app.route('/insert_tweet', methods=['POST'])
+def insert_tweet():
+    tweets=mongo.db.tweets
+    tweets.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tweets'))
 
 
 
 
 ## APP INITIATION
 
-# if __name__ == '__main__':
-#     # Bind to PORT if defined, otherwise default to 5000.
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(host='0.0.0.0', port=port, debug=True) 
-
-
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)    
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True) 
+
